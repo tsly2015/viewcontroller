4e6ce7fc4e40977220633a1c85b533689acee93f
Zhen Wang <Zhen@Zhens-MacBook-Pro.local>
2015-03-14 12:52:38 -0400

    Storyboard

--- ViewController/ViewController.swift
+++ ViewController/ViewController.swift
@@ -11,31 +11,13 @@ import UIKit
 class ViewController: UIViewController {
 
     var count = 0
-    var label:UILabel!
+    @IBOutlet var label:UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        //label
-        var label = UILabel()
-        label.frame = CGRectMake(150, 150, 60, 60)
-        label.text = "0"
-
-        self.view.addSubview(label)
-        self.label = label
-
-        //Button
-        var button = UIButton()
-        button.frame = CGRectMake(150, 250, 60, 60)
-        button.setTitle("Click", forState: .Normal)
-        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
-        self.view.addSubview(button)
-
-        button.addTarget(self, action: "incrementCount", forControlEvents:
-            UIControlEvents.TouchUpInside)
     }
 
-    func incrementCount() {
+    @IBAction func incrementCount() {
         self.count++
         self.label.text = "\(self.count)"
     }
