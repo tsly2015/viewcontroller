61b882c487a242d2b7d311e37f2f7d1872a0e86a
Zhen Wang <Zhen@Zhens-MacBook-Pro.local>
2015-03-14 12:47:35 -0400

    Label and Button

--- ViewController/ViewController.swift
+++ ViewController/ViewController.swift
@@ -10,14 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var count = 0
+    var label:UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        //label
+        var label = UILabel()
+        label.frame = CGRectMake(150, 150, 60, 60)
+        label.text = "0"
+
+        self.view.addSubview(label)
+        self.label = label
+
+        //Button
+        var button = UIButton()
+        button.frame = CGRectMake(150, 250, 60, 60)
+        button.setTitle("Click", forState: .Normal)
+        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        self.view.addSubview(button)
+
+        button.addTarget(self, action: "incrementCount", forControlEvents:
+            UIControlEvents.TouchUpInside)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func incrementCount() {
+        self.count++
+        self.label.text = "\(self.count)"
     }
 
 
